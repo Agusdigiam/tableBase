f5f23a59bdcf3cc826c778ec85dfe465bf639862
Agusdigiam <agustindigiambatista@gmail.com>
2023-10-23 19:18:50 -0300

    Adding Aditionals Lines

--- src/main/java/table/practice/table/Controller/Controller.java
+++ src/main/java/table/practice/table/Controller/Controller.java
@@ -1,15 +1,33 @@
 package table.practice.table.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import table.practice.table.Model.User;
 import table.practice.table.Repository.IRepository;
+
+import java.awt.*;
+import java.util.List;
 
 @RestController
 public class Controller {
     @Autowired
     private IRepository irepository;
 
-    @GetMapping("/users")
-    public
+    @GetMapping(value = "/users")
+    public List<User> getUser(){
+        return irepository.findAll();
+    }
+
+    @PostMapping(value = "/saveuser")
+    public String saveUser(@RequestBody User user){
+        irepository.save(user);
+        return "User has been save! :)";
+    }
+    @DeleteMapping(value = "/deleteuser")
+    public String deleteUser(@RequestBody User user){
+        irepository.delete(user);
+        return "User Deleted Successfully! ";
+    }
+
+
 }
